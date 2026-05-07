f8219f32dfa2044a45d247053504c92126a3f64c
lucaspujadas <lucaspujadas@gmail.com>
2026-05-07 14:08:09 -0300

    Fix GitHub Pages deployment

--- _next/static/r1HwMDrTsWUWhx3BfJdvP/_buildManifest.js
+++ _next/static/r1HwMDrTsWUWhx3BfJdvP/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Ascensor-de-Enteros//_next/:path+",
+        "destination": "/Ascensor-de-Enteros/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
